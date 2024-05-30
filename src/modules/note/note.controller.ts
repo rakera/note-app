399532c01b2c8305f5c27f058a624dd5ee69eb95
Akera <roman.akera@gmail.com>
2024-05-30 15:45:08 +0200

    feature/note-5-notes
    add create note logic

--- src/modules/note/note.controller.ts
+++ src/modules/note/note.controller.ts
@@ -1,6 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {
+  NoteCreateOutput,
+  NoteCreateInput,
+} from '@app/types';
+import { NoteService } from '@modules/note/note.service';
+import {
+  Body,
+  Controller,
+  Post,
+} from '@nestjs/common';
 
 @Controller('notes')
 export class NoteController {
+  constructor(private readonly noteService: NoteService) {
+  }
 
+  @Post('create')
+  async createNote(@Body() note: NoteCreateInput): Promise<NoteCreateOutput> {
+    return this.noteService.createNote(note);
+  }
 }
--- src/modules/note/note.service.ts
+++ src/modules/note/note.service.ts
@@ -1,6 +1,38 @@
+import {
+  NoteCreateInput,
+  NoteCreateOutput,
+} from '@app/types';
+import { NoteEntity } from '@modules/note/note.entity';
 import { Injectable } from '@nestjs/common';
+import {
+  InjectEntityManager,
+  InjectRepository,
+} from '@nestjs/typeorm';
+import {
+  EntityManager,
+  Repository,
+} from 'typeorm';
 
 @Injectable()
 export class NoteService {
+  constructor(
+    @InjectRepository(NoteEntity)
+    private readonly noteRepository: Repository<NoteEntity>,
+    @InjectEntityManager()
+    private readonly entityManager: EntityManager,
+  ) {
+  }
 
+  async createNote(note: NoteCreateInput): Promise<NoteCreateOutput> {
+
+    const newNote: NoteEntity = this.noteRepository.create(note);
+    const savedNote: NoteEntity = await this.noteRepository.save(newNote);
+    return new NoteCreateOutput(
+      savedNote.id,
+      savedNote.userId,
+      savedNote.text,
+      savedNote.createDate,
+      savedNote.updateDate,
+    );
+  }
 }

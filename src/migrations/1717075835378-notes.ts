69eafebde6f6850c01a9b4b6625ade629488bdcd
Akera <roman.akera@gmail.com>
2024-05-30 15:43:44 +0200

    feature/note-5-notes
    refactoring migration

--- src/migrations/1717075835378-notes.ts
+++ src/migrations/1717075835378-notes.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Notes1717071203252 implements MigrationInterface {
-    name = 'Notes1717071203252'
+export class Notes1717075835378 implements MigrationInterface {
+    name = 'Notes1717075835378'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "notes" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "text" character varying NOT NULL, "create_date" TIMESTAMP(0) NOT NULL DEFAULT now(), "update_date" TIMESTAMP(0) NOT NULL DEFAULT now(), "share_id" character varying, "user_id" bigint, CONSTRAINT "UQ_30041dc98d0f4a5843d6d8072cd" UNIQUE ("share_id"), CONSTRAINT "PK_af6206538ea96c4e77e9f400c3d" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "notes" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "user_id" bigint NOT NULL, "text" character varying NOT NULL, "create_date" TIMESTAMP(0) NOT NULL DEFAULT now(), "update_date" TIMESTAMP(0) NOT NULL DEFAULT now(), "share_id" character varying, CONSTRAINT "UQ_30041dc98d0f4a5843d6d8072cd" UNIQUE ("share_id"), CONSTRAINT "PK_af6206538ea96c4e77e9f400c3d" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "notes" ADD CONSTRAINT "FK_7708dcb62ff332f0eaf9f0743a7" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 

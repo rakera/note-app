3f4dfc5d9f08246f219b86a04e8fef5b56bcf6f8
Akera <roman.akera@gmail.com>
2024-06-02 20:30:47 +0200

    feature/note-6-swagger
    add swagger

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,36 @@ import {
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  OpenAPIObject,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
+
   const host: string = configService.get<string>('APP_HOST');
   const port: number = configService.get<number>('APP_PORT');
+
   app.useGlobalPipes(new ValidationPipe());
+
+  const config: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
+    .setTitle('Note-app')
+    .setDescription('Note-app api')
+    .setVersion('1.0')
+    .addTag('API')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+    }, 'access-token')
+    .build();
+
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);

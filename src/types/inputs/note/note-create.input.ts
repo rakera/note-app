bc0978c7e761b0051aa319c4979c8e3f0b5f6f3a
Akera <roman.akera@gmail.com>
2024-06-02 20:30:47 +0200

    feature/note-6-swagger
    add swagger

--- src/types/inputs/note/note-create.input.ts
+++ src/types/inputs/note/note-create.input.ts
@@ -1,4 +1,5 @@
 import { NoteCreateInterface } from '@app/types';
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsNotEmpty,
   IsString,
@@ -6,6 +7,7 @@ import {
 
 export class NoteCreateInput implements NoteCreateInterface {
 
+  @ApiProperty({ example: 'Note text' })
   @IsString()
   @IsNotEmpty()
   public text: string;
